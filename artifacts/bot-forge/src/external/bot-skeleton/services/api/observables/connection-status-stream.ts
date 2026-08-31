5a3b0e43e8d2045b219496cbeb8a83d9ef295484
cybibian-queens <cybibian@gmail.com>
2026-08-31 09:52:25 +0300

    fix: stop persisting legacy account state

--- artifacts/bot-forge/src/external/bot-skeleton/services/api/observables/connection-status-stream.ts
+++ artifacts/bot-forge/src/external/bot-skeleton/services/api/observables/connection-status-stream.ts
@@ -1,4 +1,3 @@
-// connection-status-stream.ts (This will manage our observable stream)
 import { BehaviorSubject } from 'rxjs';
 import { TAuthData } from '@/types/api-types';
 
@@ -8,37 +7,14 @@ export enum CONNECTION_STATUS {
     UNKNOWN = 'unknown',
 }
 
-// Initial connection status will be 'unknown'
 export const connectionStatus$ = new BehaviorSubject<string>('unknown');
 export const isAuthorizing$ = new BehaviorSubject<boolean>(false);
 export const isAuthorized$ = new BehaviorSubject<boolean>(false);
 export const account_list$ = new BehaviorSubject<TAuthData['account_list']>([]);
 export const authData$ = new BehaviorSubject<TAuthData | null>(null);
 
-// Create functions to easily update status
-export const setConnectionStatus = (status: CONNECTION_STATUS) => {
-    connectionStatus$.next(status);
-};
-
-// Set the authorized status
-export const setIsAuthorized = (isAuthorized: boolean) => {
-    isAuthorized$.next(isAuthorized);
-};
-
-// Set the authorizing status
-export const setIsAuthorizing = (isAuthorizing: boolean) => {
-    isAuthorizing$.next(isAuthorizing);
-};
-
-// Set the account list
-export const setAccountList = (accountList: TAuthData['account_list']) => {
-    account_list$.next(accountList);
-};
-
-// Set the auth data
-export const setAuthData = (authData: TAuthData | null) => {
-    if (authData?.loginid) {
-        localStorage.setItem('active_loginid', authData.loginid);
-    }
-    authData$.next(authData);
-};
+export const setConnectionStatus = (status: CONNECTION_STATUS) => connectionStatus$.next(status);
+export const setIsAuthorized = (isAuthorized: boolean) => isAuthorized$.next(isAuthorized);
+export const setIsAuthorizing = (isAuthorizing: boolean) => isAuthorizing$.next(isAuthorizing);
+export const setAccountList = (accountList: TAuthData['account_list']) => account_list$.next(accountList);
+export const setAuthData = (authData: TAuthData | null) => authData$.next(authData);
